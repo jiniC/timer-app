e7af0d53ac9046e284ba1cabc0662a1f60edf20d
jiniC <jinic323@gmail.com>
2018-05-26 09:04:45 +0900

    Adding seconds to the counter

--- components/Timer/presenter.js
+++ components/Timer/presenter.js
@@ -3,9 +3,26 @@ import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import Button from '../Button';
 
 class Timer extends Component {
+	// 새로운 props 불러올 때마다 실행
+	componentWillReceiveProps(nextProps) {
+		const currentProps = this.props;
+		if(currentProps.isPlaying === false && nextProps.isPlaying === true) {
+			// start the interval
+			const timerInterval = setInterval(() => {
+				currentProps.addSecond()
+			}, 1000);
+			this.setState({
+				timerInterval
+			})
+		} else if(currentProps.isPlaying === true && nextProps.isPlaying === false) {
+			// stop the interval
+			clearInterval(this.state.timerInterval);
+		}
+		//console.log(`The current isPlaying is: ${currentProps.isPlaying} and the new isPlaying is ${nextProps.isPlaying}`);
+	}
 	render() {
 		console.log(this.props);
-        const { isPlaying, elapsedTime, timerDuration, startTimer, restartTimer } = this.props;
+        const { isPlaying, elapsedTime, timerDuration, startTimer, restartTimer, addSecond } = this.props;
 		return (
 			<View style={styles.container}>
 				<StatusBar barStyle={'light-content'} />
